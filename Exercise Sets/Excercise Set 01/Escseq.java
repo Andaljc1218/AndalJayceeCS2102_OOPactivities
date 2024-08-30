e95497168f04ac3c5652369d182dafd161a7be02
andaljaycee <163810679+Andaljc1218@users.noreply.github.com>
2024-08-30 06:31:41 +0000

    2nd

---
+++ Exercise Sets/Excercise Set 01/Escseq.java
@@ -0,0 +1,18 @@
+class escseq {
+    public static void main(String[] args) {
+        
+        int multiple = 7;
+        int count = 5;
+
+        for (int i = 1; i <= count; i++) {
+            
+            if (i % 2 == 0) {
+                System.out.println("\t" + (multiple * i));
+            }
+            else 
+            {
+                System.out.println(multiple * i);
+            }
+        }
+    }
+}
