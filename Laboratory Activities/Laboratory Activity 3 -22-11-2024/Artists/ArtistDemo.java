9f632ee8a56dd7e07f2d44da5e2382413e507484
andaljaycee <163810679+Andaljc1218@users.noreply.github.com>
2024-11-22 08:28:51 +0000

    new

--- Laboratory Activities/Laboratory Activity 3 -22-11-2024/Artists/ArtistDemo.java
+++ Laboratory Activities/Laboratory Activity 3 -22-11-2024/Artists/ArtistDemo.java
@@ -10,27 +10,27 @@ public class ArtistDemo {
             
 
         // Artist Instance
-        Artist artist = new Artist("Kai Guatelara", "Chinese", 22, "Dance");
+        Artist artist = new Artist("Kai Guatelara", null, 22, "Dance");
         artist.displayInfo();
         System.out.println();
 
         // Singer instance
-        Singer singer = new Singer("Dionela", "Filipo", 25, "Music", Singer.Genre.POP);
+        Singer singer = new Singer("Dionela", null, 25, "Music", Singer.Genre.POP);
         singer.displayInfo();
         System.out.println();
 
         // Painter instance
-        Painter painter = new Painter("Leonardo Da Vinci", "Italian", 67, "Painting", Painter.Medium.OIL);
+        Painter painter = new Painter("Leonardo Da Vinci", null, 67, "Painting", Painter.Medium.OIL);
         painter.displayInfo();
         System.out.println();
 
         // Writer instance
-        Writer writer = new Writer("Jang Su-rak", "Korean", 40, "Writing", Writer.WritingStyle.FICTION);
+        Writer writer = new Writer("Jang Su-rak", null, 40, "Writing", Writer.WritingStyle.FICTION);
         writer.displayInfo();
         System.out.println();
 
         // Dancer instance
-        Dancer dancer = new Dancer("Joseph Lubi","Filipino", 21, "Dance", Dancer.DanceStyle.HIPHOP);
+        Dancer dancer = new Dancer("Joseph Lubi",null, 21, "Dance", Dancer.DanceStyle.HIPHOP);
         dancer.displayInfo();
     }
 }
