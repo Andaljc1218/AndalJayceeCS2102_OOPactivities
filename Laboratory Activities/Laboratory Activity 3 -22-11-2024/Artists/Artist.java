94e991217be1ff241a77db9304dcb0cdc8b440fc
andaljaycee <163810679+Andaljc1218@users.noreply.github.com>
2024-11-22 08:27:21 +0000

    new

--- Laboratory Activities/Laboratory Activity 3 -22-11-2024/Artists/Artist.java
+++ Laboratory Activities/Laboratory Activity 3 -22-11-2024/Artists/Artist.java
@@ -17,28 +17,28 @@ public class Artist {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getNationality() {
         return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getSpecialty() {
         return specialty;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void setSpecialty(String specialty) {
--- Laboratory Activities/Laboratory Activity 3 -22-11-2024/Artists/ArtistDemo.java
+++ Laboratory Activities/Laboratory Activity 3 -22-11-2024/Artists/ArtistDemo.java
@@ -4,13 +4,18 @@ public class ArtistDemo {
     public static void main(String[] args) {
         // Ma'am, some of my inputs po are imbento lang po huhu 
 
+        /* Ma'am, About po sa nationality kasama po kasi sya sa Artist pero wala po sa display info. Bali ni-follow ko lang po
+        yung nasa repo nyo po na expected/sample outputs and hindi ko na po inalis nationality sa every class gawa po nung 
+        Singer.java nyo po na example. Yun lang po thankyouu po*/ 
+            
+
         // Artist Instance
         Artist artist = new Artist("Kai Guatelara", "Chinese", 22, "Dance");
         artist.displayInfo();
         System.out.println();
 
         // Singer instance
-        Singer singer = new Singer("Dionela", "Filipino", 25, "Music", Singer.Genre.POP);
+        Singer singer = new Singer("Dionela", "Filipo", 25, "Music", Singer.Genre.POP);
         singer.displayInfo();
         System.out.println();
 
@@ -25,7 +30,7 @@ public class ArtistDemo {
         System.out.println();
 
         // Dancer instance
-        Dancer dancer = new Dancer("Joseph Lubi", "Filipino", 21, "Dance", Dancer.DanceStyle.HIPHOP);
+        Dancer dancer = new Dancer("Joseph Lubi","Filipino", 21, "Dance", Dancer.DanceStyle.HIPHOP);
         dancer.displayInfo();
     }
 }
